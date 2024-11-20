4a2ab48e6515e8ef69cb0c23f38e848e520f6c43
ADITYAAV80 <adityaav80@gmail.com>
2024-11-20 12:51:13 +0530

    login handling backend completed

--- project/src/main/java/com/aditya/project/Security/SecurityConfig.java
+++ project/src/main/java/com/aditya/project/Security/SecurityConfig.java
@@ -1,81 +1,32 @@
-//package com.aditya.project.Security;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-////
-//    @Autowired
-//    private UserDetailsService userDetailsService;
-////
-////    @Autowired
-////    private JwtFilter jwtFilter;
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//        //disable csrf
-//        http.csrf(AbstractHttpConfigurer::disable);
-//        //no one accesses the page without authentication
-//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-//        //shows login form for browser
-//        http.formLogin(Customizer.withDefaults());
-//        //for postman [basically rest APIs]
-//        http.httpBasic(Customizer.withDefaults());
-//        //making http stateless [now it remembers]
-//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//
-//        UserDetails user1 = User
-//                .withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("user")
-//                .roles("USER")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user1);
-//    }
-//}
-//
-////    @Bean
-////    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
-////        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-////        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
-////        provider.setUserDetailsService(userDetailsService);
-////        return provider;
-////     }
-////
-////    @Bean
-////    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-////        return config.getAuthenticationManager();
-////
-////
-////}
+package com.aditya.project.Security;
 
 
+import com.aditya.project.helper.RequestInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig /*implements WebMvcConfigurer*/ {
+
+    /*private final RequestInterceptor requestInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login/**")
+                .excludePathPatterns("/register/**")
+        ;
+    }*/
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
